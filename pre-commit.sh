b60fb8762a15b97309f0b4772dc5157850bbf92e
Nicholas Cain <nicholasc@alleninstitute.org>
2015-11-12 23:42:01 +0000

    OK

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,16 +1,9 @@
-PACKAGE_NAME=`python setup.py --name`
-CONDA_ENV_NAME=test_$PACKAGE_NAME
+
 
 # Stash changes to ensure code outside of commit is not tested
 git stash -q --keep-index
 
-source activate $CONDA_ENV_NAME
-RESULT=$?
-[ $RESULT -ne 0 ] && conda create -n $CONDA_ENV_NAME --file requirements.txt
-[ $RESULT -ne 0 ] && source activate $CONDA_ENV_NAME
-[ $RESULT -ne 1 ] && conda install -n $CONDA_ENV_NAME --file requirements.txt
-
-py.test
+./test.sh
 RESULT=$?
 
 # Unstash
