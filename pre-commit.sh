653594af6003ed7b7a1b9ae535bf17ecb652fe37
Nicholas Cain <nicholasc@alleninstitute.org>
2015-11-12 19:22:11 +0000

    update

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,7 +6,7 @@ RESULT=$?
 
 # Unstash
 git stash pop -q
-[ $RESULT -ne 0 ] && echo "Testing failed, aborting commit. (Use git commit --no-verify to skip tests)"
+[ $RESULT -ne 0 ] && echo "Testing failed, aborting commit."
 [ $RESULT -ne 0 ] && exit 1
 exit 0
 
