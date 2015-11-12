39c7d54d6b9c740375cc0d3b0315eb43187c07ff
Nicholas Cain <nicholasc@alleninstitute.org>
2015-11-12 23:23:00 +0000

    OK

--- template_package/test/test_basic.py
+++ template_package/test/test_basic.py
@@ -1,9 +1,12 @@
 
 def test_basic():
-    import scipy
-    
-    assert True
-    
+
+    import sys
+    import os
+    sys.path.append(os.path.join(sys.path[0],'../..'))
+    print sys.path
+    import template_package as tp
+    reload(tp)
     
 if __name__ == '__main__':                                    # pragma: no cover
     test_basic()                                              # pragma: no cover
