d145d049ce73aa56cffcde981391f6a34de5cc75
Nicholas Cain <nicholasc@alleninstitute.org>
2015-11-12 22:50:26 +0000

    OK

--- pre-commit.sh
+++ pre-commit.sh
@@ -8,7 +8,7 @@ source activate $CONDA_ENV_NAME
 RESULT=$?
 [ $RESULT -ne 0 ] && conda create -n $CONDA_ENV_NAME --file requirements.txt
 [ $RESULT -ne 0 ] && source activate $CONDA_ENV_NAME
-[ $RESULT -ne 1 ] && conda install -n $CONDA_ENV_NAME --file requirements.txt
+[ $RESULT -ne 1 ] && conda install  -n $CONDA_ENV_NAME --file requirements.txt
 
 py.test
 RESULT=$?
